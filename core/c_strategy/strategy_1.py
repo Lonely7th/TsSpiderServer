6c61c8ed2f86faee32356e2653f234d42fbaba1e
Lonely7th <35439521+Lonely7th@users.noreply.github.com>
2018-04-16 23:17:23 +0800

    增加code列表

--- core/c_strategy/strategy_1.py
+++ core/c_strategy/strategy_1.py
@@ -3,24 +3,34 @@
 import requests
 import pandas as pd
 import numpy as np
+import os
 
 
 def load_data(code):
     url = "http://47.95.243.173/tkdata?code=" + code
     req = requests.get(url).json()
-    data_list = [eval(x) for x in req["data"][1:-1].replace("}, {", "}#{").split("#")]
+    data_list = list(reversed([eval(x) for x in req["data"][1:-1].replace("}, {", "}#{").split("#")]))
     data_df = pd.DataFrame(data_list)
     return data_df
 
 
 def fun_decision(data):
-    print(data["cur_close_price"])
+    print(data)
 
 
-def profit_expectation():
-    pass
+def profit_expectation(index, price_list, period=7):
+    cur_profit = 0
+    if index + period < len(price_list):
+        cur_profit = (price_list[index+period]-price_list[index]) / price_list[index]
+    return cur_profit
 
 
 if __name__ == "__main__":
-    tk_dict = load_data("000001")
-    fun_decision(tk_dict)
+    file_path = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/bean/data_code.txt"
+    code_file = open(file_path, mode='r', encoding='utf-8')
+    while True:
+        line = code_file.readline()
+        if '' == line:
+            break
+        str_code = line.split()[0][:6]
+        fun_decision(load_data(str_code))
