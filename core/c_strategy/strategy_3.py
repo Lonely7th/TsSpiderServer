fe4351befd62c4272a44ebf1bfa1780e375cff2f
zhangjianing <jianing1@eastmoney.com>
2018-04-18 09:56:45 +0800

    策略3发布版

--- core/c_strategy/strategy_3.py
+++ core/c_strategy/strategy_3.py
@@ -19,7 +19,7 @@ def cmp_datatime(item):
 
 class TsStrategy3:
     def __init__(self):
-        pass
+        self.db_manager_wm = DBManager("wm_details")
 
     def get_result(self, ticker):
         if isinstance(ticker, tkWMacdBean) and len(ticker.get_wmacd_list()) > 30:
@@ -37,19 +37,58 @@ class TsStrategy3:
         for item in code_list:
             try:
                 code = item["code"]
+                price_list = list()
+                tur_list = list()
+                highest_list = list()
+                open_list = list()
                 # 获取wmacd数据
-                price_list = list(reversed([float(x) for x in line.split("#")[0].strip()[13:-2].split(",")]))
-                tur_list = list(reversed([float(x) for x in line.split("#")[1].strip()[1:-2].split(",")]))
-                highest_list = list(reversed([float(x) for x in line.split("#")[2].strip()[1:-2].split(",")]))
-                open_list = list(reversed([float(x) for x in line.split("#")[3].strip()[1:-2].split(",")]))
-                wmacd_list, diff_list, dea_list = fun_02(price_list[:])
-                tk_bean = tkWMacdBean(line[:11], price_list, wmacd_list, diff_list, dea_list, tur_list, highest_list, open_list)
+                tk_details = self.db_manager_wm.find_by_key({"code": code})[0]
+                for tk_item in tk_details["price_list"]:
+                    if datetime.datetime.strptime(date, "%Y-%m-%d") >= datetime.datetime.strptime(tk_item["frist_date"], "%Y-%m-%d"):
+                        price_list.append(tk_item["close_price"])
+                        tur_list.append(tk_item["close_price"])
+                        highest_list.append(tk_item["close_price"])
+                        open_list.append(tk_item["close_price"])
+                wmacd_list, diff_list, dea_list = self.get_w_macd(price_list[:])
+                # 创建wmacd实体
+                tk_bean = tkWMacdBean(code, price_list, wmacd_list, diff_list, dea_list, tur_list, highest_list, open_list)
                 if self.get_result(tk_bean):
                     buy_list.append(code)
-            except Exception:
+            except Exception as e:
+                print(str(e))
                 continue
         rm = RedisManager()
         rm.set_data("wm_" + str(date), buy_list)
+
+    # 计算当前时间段的wmacd值
+    def get_w_macd(self, price_list):
+        ema_12_list = list()
+        for index in range(len(price_list)):
+            if index == 0:
+                ema_12_list.append(price_list[0])
+            else:
+                ema_12_list.append(round(ema_12_list[index - 1] * 11 / 13 + price_list[index] * 2 / 13, 4))
+        ema_26_list = list()
+        for index in range(len(price_list)):
+            if index == 0:
+                ema_26_list.append(price_list[0])
+            else:
+                ema_26_list.append(round(ema_26_list[index - 1] * 25 / 27 + price_list[index] * 2 / 27, 4))
+        diff_list = list()
+        for index in range(len(ema_12_list)):
+            diff = ema_12_list[index] - ema_26_list[index]
+            diff_list.append(diff)
+        dea_list = list()
+        for index in range(len(diff_list)):
+            if index == 0:
+                dea_list.append(diff_list[0])
+            else:
+                dea_list.append(round(dea_list[index - 1] * 0.8 + diff_list[index] * 0.2, 4))
+        wmacd_list = list()
+        for index in range(len(dea_list)):
+            bar = (diff_list[index] - dea_list[index]) * 3
+            wmacd_list.append(bar)
+        return wmacd_list, diff_list, dea_list
 
 
 if __name__ == "__main__":
--- core/wmacd_utils.py
+++ core/wmacd_utils.py
@@ -89,36 +89,6 @@ class WmacdUtils:
                             # 修改数据库中的数据
                             self.db_manager_wm.update_wm_price_list(code, wmacd_item["frist_date"], wmacd_item)
 
-    # 计算当前时间段的wmacd值
-    def get_w_macd(self, price_list):
-        ema_12_list = list()
-        for index in range(len(price_list)):
-            if index == 0:
-                ema_12_list.append(price_list[0])
-            else:
-                ema_12_list.append(round(ema_12_list[index - 1] * 11 / 13 + price_list[index] * 2 / 13, 4))
-        ema_26_list = list()
-        for index in range(len(price_list)):
-            if index == 0:
-                ema_26_list.append(price_list[0])
-            else:
-                ema_26_list.append(round(ema_26_list[index - 1] * 25 / 27 + price_list[index] * 2 / 27, 4))
-        diff_list = list()
-        for index in range(len(ema_12_list)):
-            diff = ema_12_list[index] - ema_26_list[index]
-            diff_list.append(diff)
-        dea_list = list()
-        for index in range(len(diff_list)):
-            if index == 0:
-                dea_list.append(diff_list[0])
-            else:
-                dea_list.append(round(dea_list[index - 1] * 0.8 + diff_list[index] * 0.2, 4))
-        wmacd_list = list()
-        for index in range(len(dea_list)):
-            bar = (diff_list[index] - dea_list[index]) * 3
-            wmacd_list.append(bar)
-        return wmacd_list, diff_list, dea_list
-
 
 if __name__ == "__main__":
     wu = WmacdUtils()
