a7286596f3f1feee900a059e44fbf9fcc822bd94
Lonely7th <35439521+Lonely7th@users.noreply.github.com>
2018-04-23 22:55:53 +0800

    完善回测逻辑

--- t_bp/bp_manager.py
+++ t_bp/bp_manager.py
@@ -8,6 +8,7 @@ __mtime__ = '2018/4/3'
 import datetime
 import numpy as np
 from core.c_strategy.strategy_3 import TsStrategy3
+from mongo_db.mongodb_manager import DBManager
 
 capital_base = 1000000
 current_position = list()
@@ -25,37 +26,45 @@ def date_range(start, end, step=1, format="%Y-%m-%d"):
     return [strftime(strptime(start, format) + datetime.timedelta(i), format) for i in range(0, days, step)]
 
 
-def fun_buy(buy_list):
+def fun_buy(buy_list, date):
     global capital_base
-    # 对资金池进行均分
-    p_stage = capital_base / len(buy_list)
-    for index in buy_list:
-        # 获取开盘价(默认以每周一的开盘价开仓)
-        open_price = tk_list[index].get_open_list()
+    p_stage = capital_base / len(buy_list)  # 对资金池进行均分
+    for code in buy_list:
+        open_price = round(db_manager_tk.find_by_key({"code": code, "price_list.cur_timer": date})[0]["cur_open_price"], 2)
         if open_price and not np.isnan(open_price):
             amount = int(p_stage / open_price / 100) * 100
             if amount >= 100:
-                capital_base -= amount * open_price
-                current_position.append([index, open_price, amount])
+                current_position.append([code, open_price, amount])
 
 
-def fun_sell():
-    pass
+def fun_sell(date):
+    global capital_base
+    while current_position:
+        item_position = current_position.pop()
+        close_price = round(db_manager_tk.find_by_key({"code": item_position[0], "price_list.cur_timer": date})[0]["cur_close_price"], 2)
+        capital_base += (close_price - item_position[1]) * item_position[2]
+        # 统计历史数据
+        history_capital.append(capital_base)
 
 
 def start_bp():
     st3 = TsStrategy3()
+    history_capital.append(capital_base)
     # 初始化时间轴
-    date_list = date_range("2016-01-04", "2018-12-30")
+    date_list = date_range("2018-01-02", "2018-04-06")
     for index in range(len(date_list)):
         cur_date = date_list[index]
         if datetime.datetime.strptime(cur_date, "%Y-%m-%d").weekday() == 0:
-            buy_list = st3.get_buy_list(cur_date)
+            # 获取上周五的开仓列表
+            buy_list = st3.get_buy_list(date_list[index-3])
             if buy_list:
-                fun_buy(buy_list)
+                fun_buy(buy_list, cur_date)
         elif datetime.datetime.strptime(cur_date, "%Y-%m-%d").weekday() == 4:
-            fun_sell()
+            fun_sell(cur_date)
+    net_rate = (history_capital[-1] - history_capital[0]) / history_capital[0]  # 计算回测结果
+    print(round(net_rate * 100, 2), "%")
 
 
 if __name__ == "__main__":
+    db_manager_tk = DBManager("tk_details")
     start_bp()
