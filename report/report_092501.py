e35d967d21d8dc72662d39faa5e853defb24d565
zhangjianing <jianing1@eastmoney.com>
2018-09-26 17:32:45 +0800

    2018-09-26

--- report/report_092501.py
+++ report/report_092501.py
@@ -6,6 +6,11 @@ __author__ = 'JN Zhang'
 __mtime__ = '2018/9/25'
 """
 from mongo_db.mongodb_manager import DBManager
+import numpy as np
+
+fall = 0.3  # 下跌幅度
+period = 30  # 计算周期
+expect = 30  # 计算期望的周期
 
 if __name__ == "__main__":
     # 获取数据
@@ -15,10 +20,11 @@ if __name__ == "__main__":
         code = item["code"]
         print(code)
         tk_data = db_manager_wm.find_by_key({"code": code})[0]
-        tk_colse = [x["close"] for x in tk_data["price_list"] if x["close"] != 0]
-        print(tk_colse)
+        colse_list = [x["close"] for x in tk_data["price_list"] if x["close"] != 0]
+        print(colse_list)
     # 执行判断逻辑
-    
+    for index in range(period, len(colse_list)):
+        print(index)
     # 统计结果
     # 绘图
     pass
